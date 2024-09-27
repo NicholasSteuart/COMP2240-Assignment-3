758e14b1f46840c2cc0421fd7c2763440cf670b2
nicholassteuart <c3330826@uon.edu.au>
2024-09-27 20:40:33 +1000

    init commit

---
+++ A3.java
@@ -0,0 +1,13 @@
+/* COMP2240 Assignment 3
+ * File: A3.java
+ * Author: Nicholas Steuart c3330826
+ * Date Created: 27/9/24
+ * Date Last Modified: 27/9/24
+ * Description: MAIN file. Input and Output for Assignment 3
+ */
+public class A3
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
